f32c7cbf3b61b71e4db4ee70241567880ea809ea
ehe <erhe@health.ucsd.edu>
2022-08-11 15:43:43 -0400

    removed * from all values within df

--- filtering.R
+++ filtering.R
@@ -2,11 +2,25 @@
 library(tidyverse)
 library(readxl)
 
+#Import lung df (windows script)
+lungdf <- read_excel("C:/Users/erhe/Desktop/cytokine_raj/data/lungdf.xlsx")
+
 #remove character '*' from all values within df 
 #possibly perform this action manually, onto excel sheet itself 
-lungdf1<- lungdf %>% 
-        
+lungdf$`Mo IL-1b (19)`<-str_replace(lungdf$`Mo IL-1b (19)`,
+                                    "\\*","")
+#str_replace(vector,pattern to replace, replacement)
+lungdf$`Mo IL-3 (18)`<-str_replace(lungdf$`Mo IL-3 (18)`,
+                                   "\\*","")
+lungdf$`Mo IL-4 (39)`<-str_replace(lungdf$`Mo IL-4 (39)`,
+                                   "\\*","")
+lungdf$`Mo IL-10 (56)`<-str_replace(lungdf$`Mo IL-10 (56)`,
+                                   "\\*","")
+lungdf$`Mo IL-13 (37)`<-str_replace(lungdf$`Mo IL-13 (37)`, 
+                                    "\\*","")        
 
+lungdf1<- lungdf %>% 
+        mutate_if(is.character(),as.numeric())
 #Create vector for the control w respect to each cytokine, and then calculate the mean
 #This value will be subtracted from each experimental value and then % chg will be calculated 
 lungcontrol<-lungdf %>% 
